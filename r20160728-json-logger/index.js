beb260e66105ec19c5b19a9fcb68e0d5da12cdb9
wolfhesse.core <wolfgang.schuessel@gmail.com>
2016-07-28 17:45:07 +0200

    service ready

--- r20160728-json-logger/index.js
+++ r20160728-json-logger/index.js
@@ -1,11 +1,19 @@
 import { createLogger } from 'bunyan';
-import { send } from 'micro-core';
-import sleep from 'then-sleep';
+import { json, send } from 'micro-core';
 
 var log = createLogger({name: 'micro'});
 
 export default async function (req, res) {
-  await sleep(500);
-  log.info('.');
-  send(res, 200, 'Ready!\n');
+	log.info('* new request');
+	var rc = 'unprocessed';
+	const data = await json(req);
+	if ( data.data != undefined ) {
+		log.info('got data: ' + JSON.stringify(data.data));
+		rc=data.data;
+	} else {
+		log.warn('no data available');
+		rc='no data';
+	}
+	log.info('.');
+	send(res, 200, rc);
 }
