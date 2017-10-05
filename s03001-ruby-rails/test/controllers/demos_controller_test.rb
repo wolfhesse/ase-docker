bb91ea2927ce7abff01d84c39cff0a1d9aa8ce04
rogera.dd <wolfgang.schuessel@gmail.com>
2017-10-05 10:50:59 +0200

    wp

--- s03001-ruby-rails/test/controllers/demos_controller_test.rb
+++ s03001-ruby-rails/test/controllers/demos_controller_test.rb
@@ -3,5 +3,7 @@ require 'test_helper'
 class DemosControllerTest < ActionDispatch::IntegrationTest
   test 'the truth' do
     assert true
+    d = Demo.create
+    d.save
   end
 end
--- s03001-ruby-rails/test/models/demo_test.rb
+++ s03001-ruby-rails/test/models/demo_test.rb
@@ -4,4 +4,13 @@ class DemoTest < ActiveSupport::TestCase
   test 'the truth' do
     assert true
   end
+
+  test 'instantiate product' do
+    p = Demo.create
+    p.save!
+    pp p.attributes
+    pp p.id
+
+    assert true
+  end
 end
